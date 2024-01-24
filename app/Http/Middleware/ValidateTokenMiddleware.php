67acb525e330d9f8131af06600602c33df667687
afshin-phpy <hassaniiafshin@gmail.com>
2024-01-24 15:06:34 +0330

    fix(auth-api): Modify docker config and auth api url in ValidateTokenMiddleware

--- app/Http/Middleware/ValidateTokenMiddleware.php
+++ app/Http/Middleware/ValidateTokenMiddleware.php
@@ -14,7 +14,7 @@ class ValidateTokenMiddleware
 
     public function __construct()
     {
-        // $this->authApiUrl   = config("auth_service.api_url");
+        $this->authApiUrl   = "http://app1:8000";
         $this->endPointPath = "/api/token/validate";
     }
 
@@ -28,20 +28,18 @@ class ValidateTokenMiddleware
             $token = str_replace('Bearer ', '', $token);
 
             // Validate the token by sending it to the external service
-            // validate token
-            $response = Http::withToken($token)->post("http://192.168.130.52:8000".$this->endPointPath);
+            $response = Http::withToken($token)->post($this->authApiUrl . $this->endPointPath);
             if($response->failed()) {
                 return APIResource::error('Unauthorized', 401);
             }
             $data = $response->json();
 
-            // return $data['data']['user_id'];
             $request->merge(['user_id' =>   $data['data']['user_id']]);
 
             // Token is valid, proceed with the request
             return $next($request);
         } catch (\Throwable $th) {
-            return response($th->getMessage(), 401);
+            return APIResource::error($th->getMessage(), 401);
         }
     }
 }
